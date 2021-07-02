9417d075351b8f0f44be8278190f3888424bf3ff
Alex Wilson <Alexforeva94@gmail.com>
2021-07-02 14:11:15 +1000

    Completed bubble sort

---
+++ BubbleSort.rb
@@ -0,0 +1,41 @@
+def bubble_sort(array)
+    n = array.length
+    until n < 1
+        
+        # default values
+        newn = 0
+        i = 1
+        n = n - 1
+
+        # for the array length
+        n.times do
+            # if numbers are unordered, swap
+            if array[i - 1] > array[i]
+                array[i], array[i - 1]  = array[i - 1], array[i]
+                newn = i
+            end
+            # move to next number
+            i += 1
+        end
+        # show where last swap occured, to speed up processing
+        n = newn
+    end
+    # output
+    p array
+end
+
+bubble_sort([4,3,78,2,0,2])
+
+# pseudocode
+# function bubble_sort()
+#     n = length(array)
+#     until n < 1
+#         newn = 0
+#         for i = 1 to n - 1 do
+#             if array[i - 1] > array[i]
+#                 swap the two
+#                 newn = i
+#             end
+#         end
+#         n = newn
+#     end
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Includes:
 - Caesar Cipher - An interactive game wherein users can enter a string and encrypt it using the ceasar cipher encryption technique.
 - Substrings - An interactive application wherein users can enter a string which will be compared against a sample dictionary, displaying a hash which shows hte user the amount of times that words in the dictionary appeared in their entered string.
 - Stock Picker - An application that will calculate the best days to buy stock in a company, based on calculations for an array of the companies daily stock price.
+- Bubble sort - An application that will sort out numbers in an array according to an optimized bubble sort algorithm
 
 # Live
 
